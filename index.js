0ae8c6bdfa185437be0192caf44ca28e12cc2ed8
KatherineHanson <katherinemariehanson@gmail.com>
2017-09-11 15:33:13 -0700

    added greet module

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// assign to name of file you're referring to
+const greet = require('./lib/greet.js');
+
+console.log(greet());
+console.log(greet(3));
+console.log(greet('world'));

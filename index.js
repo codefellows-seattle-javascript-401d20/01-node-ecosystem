b223b7ff8bdb693be7c9bd95bf89695556b99689
Ron Barrantes <ronbarrantes@gmail.com>
2017-09-11 17:06:20 -0700

    arithmetic functions working

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const greet = require('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
 
 console.log(greet());
 console.log(greet('ron'));
+
+console.log(arithmetic.add(1,2));
+console.log(arithmetic.sub(1,2));
+
+console.log(arithmetic.add(1,'2'));
+console.log(arithmetic.sub('1',2));
 
 
